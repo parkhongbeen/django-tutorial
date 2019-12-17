fd571b8a83fdf7367ce08481ae309ed5f604b5ae
parkhongbeen <pack122@naver.com>
2019-12-17 17:19:37 +0900

    add detail

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,12 @@
+from django.contrib.admin.templatetags.admin_list import results
 from django.urls import path
+from django.views.generic import detail
 
-from .views import index
+from .views import index, detail, vote, results
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('<int:question_id>/', detail, name='detail'),
+    path('<int:question_id>/results.', results, name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
